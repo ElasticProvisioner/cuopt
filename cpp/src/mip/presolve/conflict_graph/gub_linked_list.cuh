f10535fe58ca3abf8c2401ee2d09aaf1459f36e3
akifcorduk <akifcorduk@gmail.com>
2025-11-04 06:51:59 -0800

    add some comments and file name changes

--- cpp/src/mip/presolve/conflict_graph/gub_linked_list.cuh
+++ cpp/src/mip/presolve/conflict_graph/gub_linked_list.cuh
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: Copyright (c) 2022-2025 NVIDIA CORPORATION & AFFILIATES. All rights
+ * SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights
  * reserved. SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-#include "cliques.cuh"
+#pragma once
 
 namespace cuopt::linear_programming::detail {
 
