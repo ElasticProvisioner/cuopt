bc1b81fc9fedf50d2f18b391d150e24af847b45d
Nicolas Blin <nicolas.blin7@hotmail.fr>
2026-01-16 17:18:14 +0100

    update strong branching benchmark

--- benchmarks/linear_programming/cuopt/strong_branching_benchmark.cu
+++ benchmarks/linear_programming/cuopt/strong_branching_benchmark.cu
@@ -40,6 +40,16 @@ bool is_frational(double in)
   return std::fabs(in - std::round(in)) > 1e-5;
 }
 
+template <typename T>
+void print(std::string_view const name, std::vector<T> const& container)
+{
+  std::cout << name << "=[";
+  for (auto const& item : container) {
+    std::cout << item << ",";
+  }
+  std::cout << "]\n";
+}
+
 std::tuple<cuopt::mps_parser::mps_data_model_t<int, double>, std::vector<cuopt::mps_parser::mps_data_model_t<int, double>>, std::vector<std::pair<int, double>>> create_batch_problem(const cuopt::mps_parser::mps_data_model_t<int, double>& op_problem, const cuopt::linear_programming::optimization_problem_solution_t<int, double>& solution, bool no_init_lower)
 { 
   std::vector<double> primal_sol = host_copy(solution.get_primal_solution());
@@ -124,15 +134,17 @@ void bench(
   cuopt::mps_parser::mps_data_model_t<int, double>& original_problem, // Only useful for warm start
   cuopt::mps_parser::mps_data_model_t<int, double>& batch_problem,
   std::vector<cuopt::mps_parser::mps_data_model_t<int, double>>& problems,
+  cuopt::linear_programming::optimization_problem_solution_t<int, double>& dual_simplex_solution,
   std::vector<std::pair<int, double>>& pairs,
   bool compare_with_baseline,
-  bool deterministic, // For now useless, need 13.1 for cuSparse deterministic
   bool init_primal_dual,
   bool init_step_size,
   bool init_primal_weight,
-  bool use_optimal_batch_size)
+  bool use_optimal_batch_size,
+  bool warm_start_from_dual_simplex,
+  bool project_initial_primal)
 {
-  // Important that those 2 are created before the sols
+  std::cout << "[Benchmark] Running with: " << "primal dual init: " << init_primal_dual << ", step size init: " << init_step_size << ", primal weight init: " << init_primal_weight << ", use optimal batch size: " << use_optimal_batch_size << ", warm start from dual simplex: " << warm_start_from_dual_simplex << ", project initial primal: " << project_initial_primal << std::endl;
 
   std::vector<cuopt::linear_programming::optimization_problem_solution_t<int, double>> sols;
 
@@ -146,18 +158,25 @@ void bench(
   bool needs_warm_start_solution =
       init_primal_dual || init_step_size || init_primal_weight;
 
-  if (needs_warm_start_solution)
+  if (warm_start_from_dual_simplex)
+  {
+    if (init_primal_dual) {
+      initial_primal = rmm::device_uvector<double>(dual_simplex_solution.get_primal_solution(), dual_simplex_solution.get_primal_solution().stream());
+      initial_dual = rmm::device_uvector<double>(dual_simplex_solution.get_dual_solution(), dual_simplex_solution.get_dual_solution().stream());
+    }
+  }
+  else if (needs_warm_start_solution)
   {
     // Solving the original to get its primal / dual vectors
     // Should not be necessary but weird behavior with conccurent halt is making things crash
     cuopt::linear_programming::pdlp_solver_settings_t<int, double> settings_local;
     settings_local.method = cuopt::linear_programming::method_t::PDLP;
-    settings_local.detect_infeasibility = true;
+    settings_local.hyper_params.project_initial_primal = project_initial_primal;
 
     cuopt::linear_programming::optimization_problem_solution_t<int, double> original_solution = cuopt::linear_programming::solve_lp(&handle, original_problem, settings_local);
 
     std::cout << "Original problem solved by PDLP in " << original_solution.get_additional_termination_information().solve_time << " using " << original_solution.get_additional_termination_information().number_of_steps_taken << std::endl;
-    if (init_primal_dual) {
+    if (init_primal_dual && !warm_start_from_dual_simplex) {
       initial_primal = rmm::device_uvector<double>(original_solution.get_primal_solution(), original_solution.get_primal_solution().stream());
       initial_dual = rmm::device_uvector<double>(original_solution.get_dual_solution(), original_solution.get_dual_solution().stream());
     }
@@ -180,8 +199,8 @@ void bench(
       // Should not be necessary but weird behavior with conccurent halt is making things crash
       cuopt::linear_programming::pdlp_solver_settings_t<int, double> settings_local;
       settings_local.method = cuopt::linear_programming::method_t::PDLP;
-      settings_local.detect_infeasibility = true;
       settings_local.iteration_limit = 100000;
+      settings_local.hyper_params.project_initial_primal = project_initial_primal;
 
       if (init_primal_dual)
       {
@@ -209,9 +228,9 @@ void bench(
   // Should not be necessary but weird behavior with conccurent halt is making things crash/mer
   cuopt::linear_programming::pdlp_solver_settings_t<int, double> settings_local;
   settings_local.method = cuopt::linear_programming::method_t::PDLP;
-  settings_local.detect_infeasibility = true;
   settings_local.iteration_limit = 100000;
-  constexpr bool only_upper = true;
+  settings_local.hyper_params.project_initial_primal = project_initial_primal;
+  constexpr bool only_upper = false;
 
   if (init_primal_dual)
   {
@@ -227,34 +246,34 @@ void bench(
     settings_local.set_initial_primal_weight(initial_primal_weight);
   }
 
-  auto start_batch = std::chrono::steady_clock::now();
-  cuopt::linear_programming::optimization_problem_solution_t<int, double> batch_solution(cuopt::linear_programming::pdlp_termination_status_t::NumericalError, handle.get_stream());
-  if (use_optimal_batch_size)
-  {
-    std::vector<int> fractional;
-    std::vector<double> root_soln_x;
-    for (size_t i = 0; i < pairs.size(); ++i)
+    auto start_batch = std::chrono::steady_clock::now();
+    cuopt::linear_programming::optimization_problem_solution_t<int, double> batch_solution(cuopt::linear_programming::pdlp_termination_status_t::NumericalError, handle.get_stream());
+    if (use_optimal_batch_size)
     {
-      fractional.push_back(pairs[i].first);
-      root_soln_x.push_back(pairs[i].second);
-    }
-    batch_solution = cuopt::linear_programming::batch_pdlp_solve(&handle, batch_problem, fractional, root_soln_x, settings_local);
-  }
-  else
-  {
-    for (size_t i = 0; i < pairs.size(); ++i)
-      settings_local.new_bounds.push_back({pairs[i].first, problems[i].get_variable_lower_bounds()[pairs[i].first], std::floor(pairs[i].second)});
-    if (!only_upper)
+      std::vector<int> fractional;
+      std::vector<double> root_soln_x;
       for (size_t i = 0; i < pairs.size(); ++i)
-        settings_local.new_bounds.push_back({pairs[i].first, std::ceil(pairs[i].second), problems[i].get_variable_upper_bounds()[pairs[i].first]});
-    batch_solution = cuopt::linear_programming::solve_lp(&handle, batch_problem, settings_local);
-  }
-  cudaDeviceSynchronize();
-  auto end_batch = std::chrono::steady_clock::now();
-  std::cout << "Batch problem solved in " << std::chrono::duration_cast<std::chrono::milliseconds>(end_batch - start_batch).count() / 1000.0 << " seconds" << std::endl;
-  if (needs_warm_start_solution) {
-    std::cout << "Total (including warm start original PDLP solve) batch solve time: " << std::chrono::duration_cast<std::chrono::milliseconds>(end_batch - start).count() / 1000.0 << " seconds" << std::endl;
-  }
+      {
+        fractional.push_back(pairs[i].first);
+        root_soln_x.push_back(pairs[i].second);
+      }
+      batch_solution = cuopt::linear_programming::batch_pdlp_solve(&handle, batch_problem, fractional, root_soln_x, settings_local);
+    }
+    else
+    {
+      for (size_t i = 0; i < pairs.size(); ++i)
+        settings_local.new_bounds.push_back({pairs[i].first, problems[i].get_variable_lower_bounds()[pairs[i].first], std::floor(pairs[i].second)});
+      if (!only_upper)
+        for (size_t i = 0; i < pairs.size(); ++i)
+          settings_local.new_bounds.push_back({pairs[i].first, std::ceil(pairs[i].second), problems[i].get_variable_upper_bounds()[pairs[i].first]});
+      batch_solution = cuopt::linear_programming::solve_lp(&handle, batch_problem, settings_local, true, false);
+    }
+    cudaDeviceSynchronize();
+    auto end_batch = std::chrono::steady_clock::now();
+    std::cout << "Batch problem solved in " << std::chrono::duration_cast<std::chrono::milliseconds>(end_batch - start_batch).count() / 1000.0 << " seconds using " << batch_solution.get_additional_termination_information().number_of_steps_taken << " steps" << std::endl;
+    if (needs_warm_start_solution) {
+      std::cout << "Total (including warm start original PDLP solve) batch solve time: " << std::chrono::duration_cast<std::chrono::milliseconds>(end_batch - start).count() / 1000.0 << " seconds" << std::endl;
+    }
 
   if (compare_with_baseline)
   {
@@ -275,7 +294,6 @@ int main(int argc, char* argv[])
   const raft::handle_t handle_;
 
   cuopt::linear_programming::pdlp_solver_settings_t<int, double> settings;
-  settings.detect_infeasibility = true;
 
 
   // Setup up RMM memory pool
@@ -299,16 +317,65 @@ int main(int argc, char* argv[])
     settings.method = cuopt::linear_programming::method_t::DualSimplex;
 
     // Solve LP problem
-    cuopt::linear_programming::optimization_problem_solution_t<int, double> solution =
-    cuopt::linear_programming::solve_lp(&handle_, op_problem, settings);
+    cuopt::linear_programming::optimization_problem_solution_t<int, double> solution(cuopt::linear_programming::pdlp_termination_status_t::NumericalError, handle_.get_stream());
+    
+    // If it doesn't already exist, dump the primal and dual solution
+    if (!std::filesystem::exists("primal_solution_" + op_problem.get_problem_name() + ".txt")) {
+      solution = cuopt::linear_programming::solve_lp(&handle_, op_problem, settings);
+      std::cout << "Original problem solved in " << solution.get_additional_termination_information().solve_time << " and " << solution.get_additional_termination_information().number_of_steps_taken << " steps" << std::endl;
+        const auto primal_solution = host_copy(solution.get_primal_solution());
+        const auto dual_solution = host_copy(solution.get_dual_solution());
+        std::ofstream file1("primal_solution_" + op_problem.get_problem_name() + ".txt");
+        for (const auto& value : primal_solution)
+          file1 << value << std::endl;
+        file1.close();
+        std::ofstream file2("dual_solution_" + op_problem.get_problem_name() + ".txt");
+        for (const auto& value : dual_solution)
+          file2 << value << std::endl;
+        file2.close();
+      }
 
-    std::cout << "Original problem solved in " << solution.get_additional_termination_information().solve_time << " and " << solution.get_additional_termination_information().number_of_steps_taken << " steps" << std::endl;
+    // Load the primal and dual solution from the files
+    std::ifstream file1("primal_solution_" + op_problem.get_problem_name() + ".txt");
+    std::ifstream file2("dual_solution_" + op_problem.get_problem_name() + ".txt");
+    std::vector<double> primal_solution;
+    std::vector<double> dual_solution;
+    for (std::string line; std::getline(file1, line);)
+      primal_solution.push_back(std::stod(line));
+    for (std::string line; std::getline(file2, line);)
+      dual_solution.push_back(std::stod(line));
+    file1.close();
+    file2.close();
+
+    // Fill the primal and dual solution in the solution object
+    solution.get_primal_solution().resize(primal_solution.size(), handle_.get_stream());
+    solution.get_dual_solution().resize(dual_solution.size(), handle_.get_stream());
+    raft::copy(solution.get_primal_solution().data(), primal_solution.data(), primal_solution.size(), handle_.get_stream());
+    raft::copy(solution.get_dual_solution().data(), dual_solution.data(), dual_solution.size(), handle_.get_stream());
 
     // Create a list of problems for each variante and update op_problem to batchify it
     auto [batch_problem, problems, pairs] = create_batch_problem(op_problem, solution, false);
 
-    bench(handle_, op_problem, batch_problem, problems, pairs, compare_with_baseline, false /*deterministic*/, true /*primal dual*/, true /*step size*/, false /*primal weight*/, true /*use optimal batch size*/);
-    //bench(handle_, op_problem, batch_problem, problems, pairs, compare_with_baseline, false /*deterministic*/, true /*primal dual*/, true /*step size*/, false /*primal weight*/, true /*use optimal batch size*/);
+    std::vector<bool> primal_dual_init = {true, false};
+    std::vector<bool> step_size_init = {true, false};
+    std::vector<bool> primal_weight_init = {true, false};
+    std::vector<bool> use_optimal_batch_size = {true, false};
+    std::vector<bool> warm_start_from_dual_simplex = {true, false};
+    std::vector<bool> project_initial_primal = {true, false};
+
+    for (const auto& primal_dual_init : primal_dual_init) {
+      for (const auto& step_size_init : step_size_init) {
+        for (const auto& primal_weight_init : primal_weight_init) {
+          for (const auto& use_optimal_batch_size : use_optimal_batch_size) {
+            for (const auto& warm_start_from_dual_simplex : warm_start_from_dual_simplex) {
+              for (const auto& project_initial_primal : project_initial_primal) {
+                bench(handle_, op_problem, batch_problem, problems, solution, pairs, compare_with_baseline, primal_dual_init, step_size_init, primal_weight_init, use_optimal_batch_size, warm_start_from_dual_simplex, project_initial_primal);
+              }
+            }
+          }
+        }
+      }
+    }
   }
 
   return 0;
